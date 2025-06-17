752932c1f180b6d3e40a655fc099d7cae6b2bbe4
Jaehuck <gottkf0919@gmail.com>
2025-06-17 22:23:38 +0900

    채팅 시스템 기본틀 생성

---
+++ Source/TeamProject/Public/UI/SmartPhone/ChattingRoomListSlot.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ChattingRoomListSlot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAMPROJECT_API UChattingRoomListSlot : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
